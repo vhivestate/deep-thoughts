6bba5595e4ad54487c0ff951c40ec7de66ef7720
vhivestate <garcia.valeria001@gmail.com>
2022-08-11 23:26:08 -0600

    add

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,19 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
+const { User, Thought } = require('../models');
 
 // create our typeDefs
 const typeDefs = gql`
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+  }
+
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
   }
 `;
 

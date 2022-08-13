612cdcb3a02e2fbfc14a6dbb258498dc89ca4058
vhivestate <garcia.valeria001@gmail.com>
2022-08-13 16:53:40 -0600

    web token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -31,8 +32,9 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
           
-            return user;
+            return { token, user };
           },
           login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
@@ -47,7 +49,8 @@ const resolvers = {
               throw new AuthenticationError('Incorrect credentials');
             }
           
-            return user;
+            const token = signToken(user);
+            return { token, user };
           }
       }
   };

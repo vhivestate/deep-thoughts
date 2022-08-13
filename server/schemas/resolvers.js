a44856b582bcea1893b7d9c8e45788df2c69b2b4
vhivestate <garcia.valeria001@gmail.com>
2022-08-12 23:48:58 -0600

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,13 @@ const resolvers = {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
           },
+          // place this inside of the `Query` nested object right after `thoughts`
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        }
     }
+
+    
   };
   
   module.exports = resolvers;

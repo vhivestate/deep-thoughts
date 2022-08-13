de506fdfc4cb3f0c4d74579d295d73ddf385740f
vhivestate <garcia.valeria001@gmail.com>
2022-08-13 16:28:58 -0600

    login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 
 const resolvers = {
@@ -33,9 +34,21 @@ const resolvers = {
           
             return user;
           },
-        login: async () => {
-    
-        }
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            return user;
+          }
       }
   };
   

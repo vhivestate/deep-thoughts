ddd81de32d8bb0af0d580ba31f37f8e0a26ecf88
vhivestate <garcia.valeria001@gmail.com>
2022-08-14 20:03:45 -0600

    thought component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,7 @@ import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
 import { ADD_FRIEND } from '../utils/mutations';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
   const [addFriend] = useMutation(ADD_FRIEND);
@@ -68,6 +69,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
